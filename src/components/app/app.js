82f324094278596725f0bc1229920a0e9c1e7188
Alex <youzhny.foto@gmail.com>
2022-10-15 12:45:38 +0300

    create data base

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+  const data = [
+    {name: 'Alex Burn', salary: 800, increase: false},
+    {name: 'Cici DiAngelo', salary: 1200, increase: true},
+    {name: 'Igor Fakeoff', salary: 2000, increase: false},
+  ];
+
   return (
     <div className="app">
         <AppInfo />
@@ -16,7 +23,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );
